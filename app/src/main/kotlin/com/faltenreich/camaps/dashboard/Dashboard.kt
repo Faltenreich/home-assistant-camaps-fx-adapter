8f2733fe78e7bff95013ab237bebe8cb83c76ea8
Faltenreich <philipp.fahlteich@gmail.com>
2024-12-16 20:46:47 +0100

    Introduce CamApsFxDashboard

--- app/src/main/kotlin/com/faltenreich/camaps/dashboard/Dashboard.kt
+++ app/src/main/kotlin/com/faltenreich/camaps/dashboard/Dashboard.kt
@@ -2,17 +2,14 @@ package com.faltenreich.camaps.dashboard
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.faltenreich.camaps.camaps.BloodSugar
-import com.faltenreich.camaps.camaps.CamApsFxState
+import com.faltenreich.camaps.dashboard.camaps.CamApsFxDashboard
 import com.faltenreich.camaps.dashboard.homeassistant.HomeAssistantDashboard
 
 @Composable
@@ -31,33 +28,9 @@ fun Dashboard(
             state = state.homeAssistant,
             modifier = Modifier.fillMaxWidth().weight(1f),
         )
-        Row(
+        CamApsFxDashboard(
+            state = state.camApsFx,
             modifier = Modifier.fillMaxWidth().weight(1f),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = "CamAPS FX",
-                modifier = Modifier.weight(1f),
-            )
-            when (val state = state.camApsFx) {
-                is CamApsFxState.None -> Text("-")
-                is CamApsFxState.Value -> {
-                    Text(state.bloodSugar.mgDl.toString())
-                    Text(
-                        when (state.bloodSugar.trend) {
-                            BloodSugar.Trend.RISING_FAST -> "+++"
-                            BloodSugar.Trend.RISING -> "++"
-                            BloodSugar.Trend.RISING_SLOW -> "+"
-                            BloodSugar.Trend.STEADY -> ""
-                            BloodSugar.Trend.DROPPING_SLOW -> "-"
-                            BloodSugar.Trend.DROPPING -> "--"
-                            BloodSugar.Trend.DROPPING_FAST -> "---"
-                            null -> ""
-                        }
-                    )
-                }
-                is CamApsFxState.Error -> Text("ERROR: ${state.message}")
-            }
-        }
+        )
     }
 }
---
+++ app/src/main/kotlin/com/faltenreich/camaps/dashboard/camaps/CamApsFxDashboard.kt
@@ -0,0 +1,58 @@
+package com.faltenreich.camaps.dashboard.camaps
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.faltenreich.camaps.camaps.BloodSugar
+import com.faltenreich.camaps.camaps.CamApsFxState
+
+@Composable
+fun CamApsFxDashboard(
+    state: CamApsFxState,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "CamAPS FX",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        when (state) {
+            is CamApsFxState.None -> Text("-")
+            is CamApsFxState.Value -> {
+                Text(state.bloodSugar.mgDl.toString())
+                Text(
+                    when (state.bloodSugar.trend) {
+                        BloodSugar.Trend.RISING_FAST -> "+++"
+                        BloodSugar.Trend.RISING -> "++"
+                        BloodSugar.Trend.RISING_SLOW -> "+"
+                        BloodSugar.Trend.STEADY -> ""
+                        BloodSugar.Trend.DROPPING_SLOW -> "-"
+                        BloodSugar.Trend.DROPPING -> "--"
+                        BloodSugar.Trend.DROPPING_FAST -> "---"
+                        null -> ""
+                    }
+                )
+            }
+            is CamApsFxState.Error -> Text("ERROR: ${state.message}")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview(
+    @PreviewParameter(CamApsFxStatePreviewParameterProvider ::class)
+    state: CamApsFxState,
+) {
+    CamApsFxDashboard(state)
+}
