f8e670d65fa5269fe4d0ec16bad8043b57822741
Faltenreich <philipp.fahlteich@gmail.com>
2024-12-17 21:19:47 +0100

    Remove obsolete Dimensions

--- app/src/main/kotlin/com/faltenreich/camaps/dashboard/Dimensions.kt
+++ app/src/main/kotlin/com/faltenreich/camaps/dashboard/Dimensions.kt
@@ -7,14 +7,7 @@ object Dimensions {
 
     object Padding {
 
-        val P_2: Dp = 2.dp
         val P_4: Dp = 4.dp
         val P_8: Dp = 8.dp
-    }
-
-    object Size {
-
-        val ICON: Dp = 60.dp
-        val ARROW: Dp = 40.dp
     }
 }
