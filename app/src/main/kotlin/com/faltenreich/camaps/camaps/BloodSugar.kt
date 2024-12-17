273d28c65d4019d7bfd1fcd53ed1182cddbcf89e
Faltenreich <philipp.fahlteich@gmail.com>
2024-12-17 18:44:47 +0100

    Add resource identifier for slowly rising trend

--- app/src/main/kotlin/com/faltenreich/camaps/camaps/BloodSugar.kt
+++ app/src/main/kotlin/com/faltenreich/camaps/camaps/BloodSugar.kt
@@ -8,7 +8,7 @@ data class BloodSugar(
     enum class Trend(val camApsImageResourceId: Int) {
         RISING_FAST(-1), // TODO
         RISING(-1), // TODO
-        RISING_SLOW(-1), // TODO
+        RISING_SLOW(2131230954),
         STEADY(2131230951),
         DROPPING_SLOW(2131230942),
         DROPPING(-1), // TODO
