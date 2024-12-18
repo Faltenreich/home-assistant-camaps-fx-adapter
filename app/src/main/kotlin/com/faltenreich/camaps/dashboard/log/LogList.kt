d4c8b104c3e34b2d65a95272ec011639e4360a0c
Faltenreich <philipp.fahlteich@gmail.com>
2024-12-18 18:42:40 +0100

    Scroll log to end on incoming entry

--- app/src/main/kotlin/com/faltenreich/camaps/dashboard/log/LogList.kt
+++ app/src/main/kotlin/com/faltenreich/camaps/dashboard/log/LogList.kt
@@ -2,7 +2,9 @@ package com.faltenreich.camaps.dashboard.log
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -10,7 +12,16 @@ fun LogList(
     entries: List<LogEntry>,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(modifier = modifier) {
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(entries) {
+        listState.scrollToItem(entries.lastIndex)
+    }
+
+    LazyColumn(
+        modifier = modifier,
+        state = listState,
+    ) {
         items(entries) { entry ->
             LogListItem(entry)
         }
