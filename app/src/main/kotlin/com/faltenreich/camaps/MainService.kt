4100e822c33cc58ae332f3dde7673e992ccc9296
Faltenreich <philipp.fahlteich@gmail.com>
2024-12-18 18:57:57 +0100

    Simplify MainService

--- app/src/main/kotlin/com/faltenreich/camaps/MainService.kt
+++ app/src/main/kotlin/com/faltenreich/camaps/MainService.kt
@@ -1,8 +1,5 @@
 package com.faltenreich.camaps
 
-import android.content.ComponentName
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import com.faltenreich.camaps.camaps.CamApsFxController
@@ -17,54 +14,19 @@ class MainService : NotificationListenerService() {
     private val camApsFxController = CamApsFxController()
     private val homeAssistantController = HomeAssistantController()
 
-    private var componentName: ComponentName? = null
-
     private val job = SupervisorJob()
     private val scope = CoroutineScope(Dispatchers.IO + job)
 
-    override fun onCreate() {
-        super.onCreate()
+    override fun onListenerConnected() {
+        super.onListenerConnected()
         scope.launch {
             homeAssistantController.start()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        job.cancel()
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        super.onStartCommand(intent, flags, startId)
-        if (componentName == null) {
-            componentName = ComponentName(this, this::class.java)
-        }
-        componentName?.let {
-            requestRebind(it)
-            toggleNotificationListenerService(it)
-        }
-        return START_REDELIVER_INTENT
-    }
-
-    private fun toggleNotificationListenerService(componentName: ComponentName) {
-        packageManager.setComponentEnabledSetting(
-            componentName,
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-            PackageManager.DONT_KILL_APP,
-        )
-        packageManager.setComponentEnabledSetting(
-            componentName,
-            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-            PackageManager.DONT_KILL_APP,
-        )
-    }
-
     override fun onListenerDisconnected() {
         super.onListenerDisconnected()
-        if (componentName == null) {
-            componentName = ComponentName(this, this::class.java)
-        }
-        componentName?.let { requestRebind(it) }
+        job.cancel()
     }
 
     override fun onNotificationPosted(statusBarNotification: StatusBarNotification?) {
