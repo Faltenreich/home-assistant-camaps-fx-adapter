4658cde3416d4cf487bdc24f77ba3f11f28087eb
Danial <admin@redsparr0w.com>
2025-12-21 10:29:38 +1300

    Remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,13 +52,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.compose)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation(kotlin("reflect"))
